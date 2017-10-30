4ae111158f0b054e9c3a346d1869539cfd79c7c8
caseymiller <case.j.miller@gmail.com>
2017-10-30 12:02:12 -0700

    added more js, css

--- app.js
+++ app.js
@@ -8,4 +8,10 @@ $(document).ready(function() {
 		 .closest('.news-item')
 		 .addClass('is-liked');
 	});
+
+	$('.js-add-link').on('click', function(even) {
+		event.preventDefault();
+
+		$('.js-form').toggleClass('is-visible')
+	});
 });
